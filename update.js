e6660ef3bbe726b27ce0c4f6ab75ce193e6ebede
Judith <juligardin@yahoo.com>
2024-05-03 16:58:01 -0500

    new code

---
+++ update.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+
+const packageJsonOpenLens = require('./lens/open-lens/package.json');
+
+packageJsonOpenLens.build.publish = [{
+    url: "https://github.com/MuhammedKalkan/OpenLens/releases/download/Latest",
+    provider: "generic"
+}];
+
+packageJsonOpenLens.version = `${packageJsonOpenLens.version}-${process.env.BUILD_ID}`;
+packageJsonOpenLens.build.npmRebuild = true;
+packageJsonOpenLens.build.detectUpdateChannel = false;
+packageJsonOpenLens.build.beforeBuild = '../../build-hooks/beforeBuild';
+
+delete packageJsonOpenLens.scripts.postinstall;
+
+packageJsonOpenLens.copyright = [
+    packageJsonOpenLens.copyright,
+    '',
+    'Binary application builds @ MuhammedKalkan/OpenLens',
+    'by Muhammed Kalkan, Jan-Otto Kröpke, Ebby Peter, Xaver Lohmüller'
+].join("\r\n")
+
+console.log(`Set build version: ${packageJsonOpenLens.version}`)
+
+fs.writeFileSync('./lens/open-lens/package.json', JSON.stringify(packageJsonOpenLens, null, 2));
